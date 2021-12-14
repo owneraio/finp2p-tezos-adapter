e1d5d0654f54cf74d53de670d59a061908d5750c
Yuval Carmel <yuval.carmel@gmail.com>
2021-12-14 17:15:41 +0200

    review updates

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,6 @@ const app = express();
 const port = 3000;
 
 // Configure Express to parse incoming JSON data
-app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
 
 // Configure routes
