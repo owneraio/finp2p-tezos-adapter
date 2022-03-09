ccbda40b0ab9afcb53d234c0ebc1593d79bb91eb
Yuval Carmel <yuval.carmel@gmail.com>
2022-03-09 16:25:11 +0200

    Server hold/release (#40)

--- server/src/routes/tokens.ts
+++ server/src/routes/tokens.ts
@@ -1,37 +1,25 @@
 import * as express from 'express';
-import {
-  tokenBalanceValidator,
-  tokenIssueValidator,
-  tokenRedeemValidator,
-  tokenTransferValidator,
-} from '../validators/tokens';
 import { asyncMiddleware } from '../helpers/middleware';
 import { TokenService } from '../services/tokens';
 
 
-const TOKENS_BASE_URL = '/api/tokens';
+const TOKENS_BASE_URL = '/api/assets';
 
 export const register = (app: express.Application) => {
   /* Get token balance. */
   app.post(
-    `${TOKENS_BASE_URL}/balance`,
-    tokenBalanceValidator,
+    `${TOKENS_BASE_URL}/getBalance`,
     asyncMiddleware(async (req, res) => {
-      const { assetId, sourcePublicKey } = req.body;
-      const balance = await TokenService.GetService().balance(assetId, sourcePublicKey);
-      res.send({ quantity: balance.toString() });
+      const balance = await TokenService.GetService().balance(req.body);
+      res.send(balance);
     }),
   );
 
   /* POST issue a token for a user. */
   app.post(
     `${TOKENS_BASE_URL}/issue`,
-    tokenIssueValidator,
     asyncMiddleware(async (req, res) => {
-      const {
-        assetId, recipientPublicKey, quantity, settlementRef,
-      } = req.body;
-      const receipt = await TokenService.GetService().issue({ assetId, recipientPublicKey, quantity, settlementRef });
+      const receipt = await TokenService.GetService().issue(req.body);
       res.json(receipt);
     }),
   );
@@ -39,12 +27,8 @@ export const register = (app: express.Application) => {
   /* POST transfer token. */
   app.post(
     `${TOKENS_BASE_URL}/transfer`,
-    tokenTransferValidator,
     asyncMiddleware(async (req, res) => {
-      const {
-        assetId, nonce, sourcePublicKey, recipientPublicKey, signature, quantity, settlementRef,
-      } = req.body;
-      const receipt = await TokenService.GetService().transfer({ nonce, assetId, sourcePublicKey, recipientPublicKey, quantity, signatureTemplate : signature, settlementRef });
+      const receipt = await TokenService.GetService().transfer(req.body);
       res.json(receipt);
     }),
   );
@@ -52,22 +36,53 @@ export const register = (app: express.Application) => {
   /* POST redeem token. */
   app.post(
     `${TOKENS_BASE_URL}/redeem`,
-    tokenRedeemValidator,
     asyncMiddleware(async (req, res) => {
-      const {
-        nonce, sourcePublicKey, quantity, signature, assetId,
-      } = req.body;
-      const receipt = await TokenService.GetService().redeem({ nonce, assetId, sourcePublicKey, quantity, signatureTemplate : signature });
+      const receipt = await TokenService.GetService().redeem(req.body);
       res.json(receipt);
     }),
   );
 
   app.get(
-    `${TOKENS_BASE_URL}/getReceipt/:id`,
+    `${TOKENS_BASE_URL}/receipts/:id`,
     asyncMiddleware(async (req, res) => {
       const { id } = req.params;
       const receipt = await TokenService.GetService().getReceipt(id);
       res.json(receipt);
     }),
   );
+
+  /* POST hold token. */
+  app.post(
+    `${TOKENS_BASE_URL}/hold`,
+    asyncMiddleware(async (req, res) => {
+      const receipt = await TokenService.GetService().hold(req.body);
+      res.json(receipt);
+    }),
+  );
+
+  /* POST release token. */
+  app.post(
+    `${TOKENS_BASE_URL}/release`,
+    asyncMiddleware(async (req, res) => {
+      const receipt = await TokenService.GetService().release(req.body);
+      res.json(receipt);
+    }),
+  );
+
+  /* POST rollback token. */
+  app.post(
+    `${TOKENS_BASE_URL}/rollback`,
+    asyncMiddleware(async (req, res) => {
+      const receipt = await TokenService.GetService().rollback(req.body);
+      res.json(receipt);
+    }),
+  );
+
+  // /* POST operation status. */
+  // app.post(
+  //     `/operations/status/:correlationId`,
+  //     asyncMiddleware(async (req, res) => {
+  //
+  //     }),
+  // );
 };
---
+++ server/src/services/model.ts
@@ -0,0 +1,487 @@
+declare namespace Components {
+  namespace Schemas {
+    export type Asset = CryptocurrencyAsset | FiatAsset | Finp2pAsset;
+    export interface Balance {
+      asset: Asset;
+      balance: string;
+    }
+    export interface CryptoWalletAccount {
+      type: 'cryptoWallet';
+      /**
+             * address of the cryptocurrency wallet
+             */
+      address: string;
+    }
+    export interface CryptocurrencyAsset {
+      type: 'cryptocurrency';
+      /**
+             * unique identifier symbol of the cryptocurrency
+             */
+      code: string;
+    }
+    export interface DepositInstruction {
+      account: /* describes destination for remote operations operations */ Destination;
+      /**
+             * description
+             */
+      description: string;
+    }
+    export interface DepositOperation {
+      /**
+             * unique correlation id which identify the operation
+             */
+      cid: string;
+      /**
+             * flag indicating if the operation completed, if true then error or response must be present (but not both)
+             */
+      isCompleted: boolean;
+      error?: DepositOperationErrorInformation;
+      response?: DepositInstruction;
+    }
+    export interface DepositOperationErrorInformation {
+    }
+    /**
+         * describes destination for remote operations operations
+         */
+    export type Destination = /* describes destination for remote operations operations */ EscrowAccount | CryptoWalletAccount | FiatAccount | FinIdAccount;
+    export interface EmptyOperation {
+      /**
+             * unique correlation id which identify the operation
+             */
+      cid: string;
+      /**
+             * flag indicating if the operation completed, if true then error or response must be present (but not both)
+             */
+      isCompleted: boolean;
+      error?: EmptyOperationErrorInformation;
+    }
+    export interface EmptyOperationErrorInformation {
+    }
+    export interface EscrowAccount {
+      type: 'escrow';
+      /**
+             * FinID of the user
+             */
+      finId: string;
+      /**
+             * escrow account id
+             */
+      escrowAccountId: string;
+    }
+    export interface FiatAccount {
+      type: 'fiatAccount';
+      /**
+             * IBAN or other code to represent a fiat account
+             */
+      code: string;
+    }
+    export interface FiatAsset {
+      type: 'fiat';
+      /**
+             * unique identifier code of the fiat currency - based on ISO-4217
+             */
+      code: string;
+    }
+    /**
+         * describing a field in the hash group
+         */
+    export interface Field {
+      /**
+             * name of field
+             */
+      name: string;
+      /**
+             * type of field
+             */
+      type: 'string' | 'int' | 'bytes';
+      /**
+             * hex representation of the field value
+             */
+      value: string;
+    }
+    export interface FinIdAccount {
+      type: 'finId';
+      finId: string;
+    }
+    export interface Finp2pAsset {
+      type: 'finp2p';
+      /**
+             * unique resource ID of the FinP2P asset
+             */
+      resourceId: string;
+    }
+    export interface HashGroup {
+      /**
+             * hex representation of the hash group hash value
+             */
+      hash: string;
+      /**
+             * list of fields by order they appear in the hash group
+             */
+      fields?: /* describing a field in the hash group */ Field[];
+    }
+    export interface OperationBase {
+      /**
+             * unique correlation id which identify the operation
+             */
+      cid: string;
+      /**
+             * flag indicating if the operation completed, if true then error or response must be present (but not both)
+             */
+      isCompleted: boolean;
+    }
+    export interface OperationStatus {
+      type: 'receipt' | 'deposit' | 'empty';
+      operation: DepositOperation | ReceiptOperation | EmptyOperation;
+    }
+    export type PayoutAsset = CryptocurrencyAsset | FiatAsset;
+    export interface Receipt {
+      /**
+             * the receipt id
+             */
+      id: string;
+      asset: Asset;
+      /**
+             * quantity of the assets
+             */
+      quantity: string;
+      /**
+             * transaction timestamp
+             */
+      timestamp?: number; // int64
+      source: Source;
+      destination: /* describes destination for remote operations operations */ Destination;
+      /**
+             * the id of related / counterpary operation
+             */
+      settlementRef?: string;
+      transactionDetails?: /* Additional input and output details for UTXO supporting DLTs */ TransactionDetails;
+    }
+    export interface ReceiptOperation {
+      /**
+             * unique correlation id which identify the operation
+             */
+      cid: string;
+      /**
+             * flag indicating if the operation completed, if true then error or response must be present (but not both)
+             */
+      isCompleted: boolean;
+      error?: ReceiptOperationErrorInformation;
+      response?: Receipt;
+    }
+    export interface ReceiptOperationErrorInformation {
+      code: number; // uint32
+      status: string;
+    }
+    /**
+         * represent a signature template information
+         */
+    export interface SignatureTemplate {
+      /**
+             * hex representation of the signature
+             */
+      signature: string;
+      /**
+             * ordered list of hash groups
+             */
+      template: {
+        hashGroups: HashGroup[];
+        /**
+                 * hex representation of the combined hash groups hash value
+                 */
+        hash: string;
+      };
+    }
+    export interface Source {
+      /**
+             * FinID, public key of the user
+             */
+      finId: string;
+      /**
+             * an optional escrow account id
+             */
+      escrowAccountId?: string;
+    }
+    /**
+         * Additional input and output details for UTXO supporting DLTs
+         */
+    export interface TransactionDetails {
+      inputs?: {
+        /**
+                 * transaction id of the input token
+                 */
+        transactionId: string;
+        /**
+                 * token input quantity
+                 */
+        quantity: string;
+        /**
+                 * index of the token in the transaction that created it
+                 */
+        index: number; // uint32
+      }[];
+      outputs?: {
+        /**
+                 * token output quantity
+                 */
+        quantity: string;
+        /**
+                 * toke destination hex representation of a secp256k1 public key 33 bytes compressed
+                 */
+        publicKey: string;
+        /**
+                 * index of the token in the transaction
+                 */
+        index: number; // uint32
+      }[];
+    }
+  }
+}
+declare namespace Paths { // eslint-disable-line @typescript-eslint/no-unused-vars
+  namespace CreateAsset {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      asset: Components.Schemas.Asset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.EmptyOperation;
+    }
+  }
+  namespace DepositInstruction {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      destination: /* describes destination for remote operations operations */ Components.Schemas.Destination;
+      owner: Components.Schemas.Source;
+      asset: Components.Schemas.Asset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.DepositOperation;
+    }
+  }
+  namespace GetAssetBalance {
+    export interface RequestBody {
+      owner: Components.Schemas.Source;
+      asset: Components.Schemas.Asset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.Balance;
+    }
+  }
+  namespace GetOperation {
+    namespace Parameters {
+      export type Cid = string;
+    }
+    export interface PathParameters {
+      cid: Parameters.Cid;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.OperationStatus;
+    }
+  }
+  namespace GetReceipt {
+    namespace Parameters {
+      export type TransactionId = string;
+    }
+    export interface PathParameters {
+      transactionId: Parameters.TransactionId;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace HoldOperation {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * nonce
+             */
+      nonce: string;
+      /**
+             * escrow operation id
+             */
+      operationId: string;
+      source: Components.Schemas.Source;
+      destination?: /* describes destination for remote operations operations */ Components.Schemas.Destination;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Asset;
+      /**
+             * expiry
+             */
+      expiry: number; // uint64
+      signature: /* represent a signature template information */ Components.Schemas.SignatureTemplate;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace IssueAssets {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * nonce
+             */
+      nonce: string;
+      destination: Components.Schemas.Source;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Finp2pAsset;
+      /**
+             * referrence to the corresponding payment operation
+             */
+      settlementRef: string;
+      signature: /* represent a signature template information */ Components.Schemas.SignatureTemplate;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace Payout {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      source: Components.Schemas.Source;
+      destination: /* describes destination for remote operations operations */ Components.Schemas.Destination;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.PayoutAsset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace RedeemAssets {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * nonce
+             */
+      nonce: string;
+      source: Components.Schemas.Source;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Finp2pAsset;
+      /**
+             * referrence to the corresponding payment operation
+             */
+      settlementRef: string;
+      signature: /* represent a signature template information */ Components.Schemas.SignatureTemplate;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace ReleaseOperation {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * escrow operation id
+             */
+      operationId: string;
+      source: Components.Schemas.Source;
+      destination: /* describes destination for remote operations operations */ Components.Schemas.Destination;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Asset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace RollbackOperation {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * escrow operation id
+             */
+      operationId: string;
+      source: Components.Schemas.Source;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Asset;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+  namespace Transfer {
+    export interface HeaderParameters {
+      'Idempotency-Key': Parameters.IdempotencyKey;
+    }
+    namespace Parameters {
+      export type IdempotencyKey = string;
+    }
+    export interface RequestBody {
+      /**
+             * nonce
+             */
+      nonce: string;
+      source: Components.Schemas.Source;
+      destination: /* describes destination for remote operations operations */ Components.Schemas.Destination;
+      /**
+             * quantity
+             */
+      quantity: string;
+      asset: Components.Schemas.Asset;
+      /**
+             * referrence to the corresponding payment operation
+             */
+      settlementRef: string;
+      signature: /* represent a signature template information */ Components.Schemas.SignatureTemplate;
+    }
+    namespace Responses {
+      export type $200 = Components.Schemas.ReceiptOperation;
+    }
+  }
+}
