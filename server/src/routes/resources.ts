ed8160dd9e4376932b48bca45e85bd72111fa175
Yuval Carmel <yuval.carmel@gmail.com>
2022-05-22 15:23:06 +0300

    update create asset url

--- server/src/routes/resources.ts
+++ server/src/routes/resources.ts
@@ -2,12 +2,12 @@ import * as express from 'express';
 import { asyncMiddleware } from '../helpers/middleware';
 import { TokenService } from '../services/tokens';
 
-const RESOURCES_BASE_URL = '/api/accounts';
+const RESOURCES_BASE_URL = '/api/assets';
 
 export const register = (app: express.Application) => {
   /* Create resource */
   app.post(
-    `${RESOURCES_BASE_URL}/createAsset`,
+    `${RESOURCES_BASE_URL}/create`,
     asyncMiddleware(async (req, res) => {
       await TokenService.GetService().onCreateAsset(req.body);
       return res.send({ isCompleted: true } as Components.Schemas.EmptyOperation);
